a736f50b18ebf40c0b1da84daca404f4de0c8d69
venkata subbaiah <venky.kandagaddala@gmail.com>
2016-11-24 20:05:47 +0530

    overriding of pluck method

--- lib/pluck_to_hash.rb
+++ lib/pluck_to_hash.rb
@@ -16,6 +16,13 @@ module PluckToHash
       end
     end
 
+    def pluck(*args)
+      args_count = args.count
+      raise ArgumentError, 'wrong number of arguments (given 0, expected at least 1)' if args_count.zero?
+      args_count == 1 ? map(&args.first) : map { |element| args.map { |arg| element.send(arg) } }
+    end
+
+
     def pluck_to_struct(*keys)
       struct_type = keys[-1].is_a?(Hash) ? keys.pop.fetch(:struct_type,Struct) : Struct
       block_given = block_given?
